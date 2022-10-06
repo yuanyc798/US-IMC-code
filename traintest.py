eedd121bac9f637da79ee6f38a27c7e4abb24f43
yuanyc798 <44064799+yuanyc798@users.noreply.github.com>
2022-10-06 12:36:44 +0800

    Update traintest.py

--- traintest.py
+++ traintest.py
@@ -51,7 +51,7 @@ def upRsize(x,img_h,img_w):
 		return tf.image.resize_images(x,(img_h,img_w),method=0)
 
 def network( inputs_img1,output_dim, is_train, alpha=0.01):
-    outputs1,outputs2,outputs3,outputs4,outputs=unet(inputs_img1,'fcn1', output_dim, is_train)
+    outputs1,outputs2,outputs3,outputs4,outputs=csmnet(inputs_img1,'fcn1', output_dim, is_train)
     return outputs1,outputs2,outputs3,outputs4,outputs
    
 def get_optimizer(g_loss,beta1=0.9, learning_rate=0.001):
