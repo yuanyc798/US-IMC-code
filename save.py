3e23b0d5193f7b3bff292f6f8a844744f88e9c7b
yuanyc798 <44064799+yuanyc798@users.noreply.github.com>
2022-12-05 17:22:49 +0800

    Update save.py

--- save.py
+++ save.py
@@ -14,14 +14,14 @@ from tensorflow.keras.losses import binary_crossentropy,categorical_crossentropy
 import time
 from loss import *
 
-def save_images(samples,fold_test,path_save,Yv_hot):
+def save_images(samples,id_test,path_save,Yv_hot):
     roctrix=np.zeros((4,30))
     isExists=os.path.exists(path_save)
     nm=10
     if not isExists:
         os.makedirs(path_save)
     for i in range(nm):
-      cv2.imwrite(path_save+str(nm*(fold_test-1)+i+1)+'.tif',np.argmax(samples[i],-1)*100)
+      cv2.imwrite(path_save+id_test[i].split('/')[-1].split('.')[0]+'.tif',np.argmax(samples[i],-1)*100)
       preds=samples[i]
       
       for k in range(1,3):#(1,3)
@@ -50,24 +50,7 @@ def save_images(samples,fold_test,path_save,Yv_hot):
         roctrix[3,m]+=FN
     return roctrix
     
-def Heatmap(sess,Va_images,inputs_img1,fold_test,path_save):
-    out_tensor= sess.graph.get_tensor_by_name('fcn1/conb:0')#.outputs[0]
-    print(out_tensor.shape)
-    out_t = sess.run(out_tensor,feed_dict={inputs_img1: Va_images})
-    nm=10
-    for i in range(nm):
-      heat=np.mean(out_t[i],axis=-1)
-      heatmap = np.maximum(heat,0)
-      heatmap /= np.max(heatmap)
-      heatmap = cv2.resize(heatmap,(512,320))
-      heatmap = np.uint8(255 * heatmap)    
-      heatmap = cv2.applyColorMap(heatmap//1, cv2.COLORMAP_JET)    
-      cv2.imwrite(path_save+str(nm*(fold_test-1)+i+1)+'h.tif',heatmap)   
-    
-def show_generator_output(sess,Va_images,inputs_img1,output_dim):
-    sp1,sp2,sp3,sp4,sp5= sess.run(network(inputs_img1,output_dim, False),feed_dict={inputs_img1: Va_images}) 
-                   
-    return sp5#(sp1+sp2+sp3+sp4+sp5)/5
+
     
      
         
